6ba75c86889bf5312aadea74dc443a7a5c167bf1
Ítalo Cássio <italocassio95@gmail.com>
2019-04-24 21:27:10 -0300

    adicionando projeto

---
+++ API/Migrations/20190423232814_campoImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class campoImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Eventos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Eventos");
+        }
+    }
+}
